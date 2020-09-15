9bd102c029877fb62dfd8d1530931374a2b2cef2
atsukofu <atsukofu0527@gmail.com>
2020-09-15 22:46:25 +0900

    add paramater

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+Route::get('hello',function(){
+    return '<html><body><h1>Hello</h1><p>This is sample page</p></body></html>';
+});
+*/
+// Route::get('hello/{msg?}',function ($msg='no message.'){
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//     <title>Hello</title>
+//     <style>
+//     body {
+//         font-size:16pt; 
+//         color:#999;
+//     }
+//     h1 {
+//         font-size:100pt;
+//         text-align: right;
+//         color:#eee;
+//         margin:-40px 0 -50px 0;
+//     }
+//     </style>    
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>{$msg}</p>
+//         <p>これはサンプルでつくったページです</p>
+//     </body>
+//     </html>
+//     EOF;
+//     return $html;
+// });
+Route::get('hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@index');
