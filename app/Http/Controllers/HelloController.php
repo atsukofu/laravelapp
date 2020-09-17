28d577f82bad05213a8665f31d62472f9175c012
atsukofu <atsukofu0527@gmail.com>
2020-09-17 22:21:14 +0900

    add index action

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,38 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
   
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<< EOF
-<style>
-body {font-size:16px; color:#999;
-}
-h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0;
-}
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';        
-function tag($tag, $txt) {
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
 class HelloController extends Controller {
-    public function index() {
-        global $head, $style, $body, $end;
-        $html = $head . tag('title','Hello/Index') . $style . $body
-        . tag('h1','Index') . tag('p','this is Index page')
-        . '<a href="/hello/other">go to Other page</a>'
-        . $end;
-        return $html;
-    }
-    public function other() {
-        global $head, $style, $body, $end;
-        $html = $head . tag('title','Hello/Other') . $style . $body
-        .tag('h1','Other') . tag('p','this is Other page')
-        .$end;
-        return $html;
+    public function index(Request $request, Response $response) {
+        return <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+            body {font-size:16pt; color:#999}
+            h1 {font-size: 120pt; text-align: right; color: #fafafa; margin: -50px 0 -120px 0;}
+        </style>
+        </head>
+        <body>
+          <h1>Hello/h1>
+          <h3>Request</h3>
+          <p>これは、シングルコントローラーのアクションです。</p>
+          <h3>Request</h3>
+          <pre>{$response}</pre>
+        </body>
+        </html>
+        EOF;
+        $response->setcontroller($html);
+        return $response;
     }
 }
 
